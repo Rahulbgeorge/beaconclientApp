c7adc3a78725f799ddad03c33d2c1d31437085db
Rahul George <rahul.bgeorge@gmail.com>
2018-06-10 20:57:39 +0530

    First commit

---
+++ app/src/main/java/com/example/rahulgeorge/beaconclientapp/Device.java
@@ -0,0 +1,23 @@
+package com.example.rahulgeorge.beaconclientapp;
+
+public class Device {
+    public String getDeivceName() {
+        return deivceName;
+    }
+
+    public void setDeivceName(String deivceName) {
+        this.deivceName = deivceName;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    public String deivceName;
+    public Boolean state;
+
+}
